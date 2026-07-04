0f2ce2d0d8d0eae17abf2321800f4b4807642142
Viktor Ferenczi <viktor@ferenczi.eu>
2026-07-04 16:04:01 +0200

    1.12.1

--- ServerPlugin/Plugin.cs
+++ ServerPlugin/Plugin.cs
@@ -22,8 +22,8 @@ using SdkLogger = PluginSdk.Logging.Logger;
 #if !DEV_BUILD
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.12.0")]
-[assembly: AssemblyFileVersion("1.12.0")]
+[assembly: AssemblyVersion("1.12.1")]
+[assembly: AssemblyFileVersion("1.12.1")]
 #endif
 
 namespace ServerPlugin;
